143a48ceb9871d272bdf8c0c1c1a3cf5f083b240
DCHoule <reynold_houle@yahoo.com>
2018-05-30 14:50:44 -0400

    first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe( 'Fried Noodles',
+                    'Awesome Thia Fried Noodles!',
+                    'https://www.maxpixel.net/static/photo/640/Thai-Food-Asian-Meal-Fried-Noodles-Noodle-518035.jpg',
+                    [
+                        new Ingredient('Beef', 1),
+                        new Ingredient('Rice Noodles', 2),
+                        new Ingredient('Chicken', 1),
+                    ]),
+        new Recipe( 'Roasted Salmon',
+                    'Amazingly healthy and delicious',
+                    'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+                    [
+                        new Ingredient('Salmon', 2),
+                        new Ingredient('Dill', 1),
+                        new Ingredient('Lemon', 2),
+
+                    ])
+    ];
+
+    constructor(private shoppingListService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToSL(ingredients: Ingredient[]) {
+        this.shoppingListService.addRecipeIngredients(ingredients);
+    }
+}
---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,32 @@
+import { EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apple', 5),
+        new Ingredient('Tomatoes', 8),
+        new Ingredient('Potatoes', 6),
+        new Ingredient('Oranges', 3)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addRecipeIngredients(ingredients: Ingredient[]) {
+        // // tslint:disable-next-line:prefer-const
+        // for (let ingredient of ingredients) {
+        //     this.addIngredient(ingredient);
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+}
